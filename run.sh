77ae2cd76a8ef761aecfe1682a269b0a66be9651
itsumura-h <dumblepy@gmail.com>
2026-03-28 19:51:48 +0900

    run basolato jester gin echo axum actix laravel django fastapi rails hono deno

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 # ./tfb --mode verify --test basolato
 # ./tfb --mode verify --test basolato --type cached-query
 # ./tfb --mode benchmark --test basolato --type cached-query
-./tfb --mode benchmark --test basolato asynchttpserver gin echo axum actix laravel django fastapi rails hono deno
+./tfb --mode benchmark --test basolato jester gin echo axum actix laravel django fastapi rails hono deno
