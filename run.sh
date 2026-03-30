4d9794dda3cdb2ce138a09c5de0d48f129996826
itsumura-h <dumblepy@gmail.com>
2026-03-31 02:45:50 +0900

    .

--- run.sh
+++ run.sh
@@ -2,3 +2,4 @@
 # ./tfb --mode verify --test basolato --type cached-query
 # ./tfb --mode benchmark --test basolato --type cached-query
 ./tfb --mode benchmark --test basolato jester gin echo axum actix laravel django fastapi rails hono deno
+# ./tfb --mode benchmark --test basolato echo
